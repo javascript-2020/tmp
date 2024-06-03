a8b1660fa7c1c681adf532eead49e22126b1fb7c
matt <javascript.12.03.2024@gmail.com>
2024-06-03 16:13:45 +0100

    update erver.js

---
+++ server.js
@@ -0,0 +1,17 @@
+
+
+        var quick   = require('https-quick').quickh();
+        quick.dir('/','');
+        quick.url('/index.html',`
+              <style>div{width:80%;height:500px}</style>
+              <div></div>
+              <script src='ace.js'></script>
+              <script>debugger;
+                    var node=document.getElementsByTagName('div')[0];
+                    var editor=ace.edit(node);
+                    editor.commands.bindKey("ctrl-shift-7", "togglecomment")
+              </script>
+        `);
+        
+        
+        
